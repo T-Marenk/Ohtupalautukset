8cee8e89e1adbd6bb7cbbbd7a981ac08f0865ac3
Tuomas Marenk <tuomas.marenk@gmail.com>
2022-11-14 13:30:51 +0200

    Riippuvuuksien injektointi osa5

--- viikko2/verkkokauppa-1/src/index.py
+++ viikko2/verkkokauppa-1/src/index.py
@@ -1,20 +1,9 @@
 from kauppa import Kauppa
-from varasto import Varasto
-from pankki import Pankki
-from viitegeneraattori import Viitegeneraattori
-from kirjanpito import Kirjanpito
+from kirjanpito import the_kirjanpito_olio
 
 
 def main():
-    viitegeneraattori = Viitegeneraattori()
-    kirjanpito = Kirjanpito()
-    varasto = Varasto(kirjanpito)
-    pankki = Pankki(kirjanpito)
-    kauppa = Kauppa(
-            varasto,
-            pankki,
-            viitegeneraattori
-            )
+    kauppa = Kauppa()
 
     # kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
     kauppa.aloita_asiointi()
@@ -33,7 +22,7 @@ def main():
     kauppa.tilimaksu("Arto Vihavainen", "3425-1652")
 
     # kirjanpito
-    for tapahtuma in kirjanpito.tapahtumat:
+    for tapahtuma in the_kirjanpito_olio.tapahtumat:
         print(tapahtuma)
 
 
