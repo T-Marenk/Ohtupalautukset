341febebf64a65209c964df1102ca72edc41cb46
Tuomas Marenk <tuomas.marenk@gmail.com>
2022-11-28 09:37:59 +0200

    Yhden tuotteen lisaamisen jalkeen korin hinta on tuotteen hinta

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -11,8 +11,14 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
 
-    def test_yden_tuotteen_lisaamisen_jalkeen_korissa_on_yksi_tuote(self):
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_on_yksi_tuote(self):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
+
+    def test_yhden_tuotteen_lisaamisen_jalkeen_korin_hinta_on_tuotteen_hinta(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(self.kori.hinta(), 3)
