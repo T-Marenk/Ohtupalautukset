a9b40a9c33f8128303724463a425f663e8aa9b39
Tuomas Marenk <tuomas.marenk@gmail.com>
2022-12-05 15:51:28 +0200

    High scores method

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -18,8 +18,18 @@ class TennisGame:
                 return "Fifteen-All"
             elif self.player1_score == 2:
                 return "Thirty-All"
-            else:
-                return "Deuce"
+            return "Deuce"
+
+    def get_winning_score_value(self):
+            minus_result = self.player1_score - self.player2_score
+
+            if minus_result == 1:
+                return "Advantage player1"
+            elif minus_result == -1:
+                return "Advantage player2"
+            elif minus_result >= 2:
+                return "Win for player1"
+            return "Win for player2"
 
     def get_score(self):
         score = ""
@@ -29,16 +39,7 @@ class TennisGame:
             score = self.get_equal_score_value()
 
         elif self.player1_score >= 4 or self.player2_score >= 4:
-            minus_result = self.player1_score - self.player2_score
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+            score = self.get_winning_score_value()
         else:
             for i in range(1, 3):
                 if i == 1:
