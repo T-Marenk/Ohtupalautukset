37d23448423499e10ce40dbcc9844375a1e9c60d
Tuomas Marenk <tuomas.marenk@gmail.com>
2022-12-05 16:04:48 +0200

    Small score values, remove hard_coded player name

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -6,50 +6,52 @@ class TennisGame:
         self.player2_score = 0
 
     def won_point(self, player_name):
-        if player_name == "player1":
+        if player_name == self.player1_name:
             self.player1_score += 1
         else:
             self.player2_score += 1
 
     def get_equal_score_value(self):
-            if self.player1_score == 0:
-                return "Love-All"
-            elif self.player1_score == 1:
-                return "Fifteen-All"
-            elif self.player1_score == 2:
-                return "Thirty-All"
-            return "Deuce"
+        if self.player1_score == 0:
+            return "Love-All"
+        elif self.player1_score == 1:
+            return "Fifteen-All"
+        elif self.player1_score == 2:
+            return "Thirty-All"
+        return "Deuce"
 
     def get_winning_score_value(self):
-            point_distribution = self.player1_score - self.player2_score
+        point_distribution = self.player1_score - self.player2_score
 
-            if point_distribution == 1:
-                return "Advantage player1"
-            elif point_distribution == -1:
-                return "Advantage player2"
-            elif point_distribution >= 2:
-                return "Win for player1"
-            return "Win for player2"
+        if point_distribution == 1:
+            return "Advantage player1"
+        elif point_distribution == -1:
+            return "Advantage player2"
+        elif point_distribution >= 2:
+            return "Win for player1"
+        return "Win for player2"
+
+    def get_small_score_value(self):
+        current_score = ""
+        for player_score in [self.player1_score, self.player2_score]:
+            if player_score == self.player2_score:
+                current_score += "-"
+
+            if player_score == 0:
+                current_score += "Love"
+            elif player_score == 1:
+                current_score += "Fifteen"
+            elif player_score == 2:
+                current_score += "Thirty"
+            elif player_score == 3:
+                current_score += "Forty"
+        return current_score
 
     def get_score(self):
+        possible_win_score = 4
 
         if self.player1_score == self.player2_score:
             return self.get_equal_score_value()
-        elif self.player1_score >= 4 or self.player2_score >= 4:
+        elif self.player1_score >= possible_win_score or self.player2_score >= possible_win_score:
             return self.get_winning_score_value()
-        else:
-            current_score = ""
-            for player_score in [self.player1_score, self.player2_score]:
-                if player_score == self.player2_score:
-                    current_score += "-"
-
-                if player_score == 0:
-                    current_score += "Love"
-                elif player_score == 1:
-                    current_score += "Fifteen"
-                elif player_score == 2:
-                    current_score += "Thirty"
-                elif player_score == 3:
-                    current_score += "Forty"
-
-            return current_score
+        return self.get_small_score_value()
