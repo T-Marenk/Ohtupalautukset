77d3def61d9afebd4810c13cf4863fd52e7b7bae
Tuomas Marenk <tuomas.marenk@gmail.com>
2022-12-06 10:13:12 +0200

    Refactorin tennis

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -12,46 +12,27 @@ class TennisGame:
             self.player2_score += 1
 
     def get_equal_score_value(self):
-        if self.player1_score == 0:
-            return "Love-All"
-        elif self.player1_score == 1:
-            return "Fifteen-All"
-        elif self.player1_score == 2:
-            return "Thirty-All"
-        return "Deuce"
+        score_values = {0: "Love-All", 1: "Fifteen-All", 2: "Thirty-All", 3: "Deuce"}
+        return score_values[min(self.player1_score,3)]
 
     def get_winning_score_value(self):
-        point_distribution = self.player1_score - self.player2_score
-
-        if point_distribution == 1:
-            return "Advantage player1"
-        elif point_distribution == -1:
-            return "Advantage player2"
-        elif point_distribution >= 2:
-            return "Win for player1"
-        return "Win for player2"
+        point_distribution = max(min(self.player1_score - self.player2_score, 2), -2)
+        point_distribution_vales = {1: "Advantage player1", -1: "Advantage player2", 2: "Win for player1", -2: "Win for player2"}
+        return point_distribution_vales[point_distribution]
 
     def get_small_score_value(self):
         current_score = ""
+        score_values = {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}
         for player_score in [self.player1_score, self.player2_score]:
             if player_score == self.player2_score:
                 current_score += "-"
-
-            if player_score == 0:
-                current_score += "Love"
-            elif player_score == 1:
-                current_score += "Fifteen"
-            elif player_score == 2:
-                current_score += "Thirty"
-            else:
-                current_score += "Forty"
+            current_score += score_values[player_score]
         return current_score
 
     def get_score(self):
         possible_win_score = 4
-
         if self.player1_score == self.player2_score:
             return self.get_equal_score_value()
-        elif self.player1_score >= possible_win_score or self.player2_score >= possible_win_score:
+        if self.player1_score >= possible_win_score or self.player2_score >= possible_win_score:
             return self.get_winning_score_value()
         return self.get_small_score_value()
