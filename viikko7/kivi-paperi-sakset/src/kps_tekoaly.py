d26e1a6a6265a9ad2a11f25559af5c42769132c6
Tuomas Marenk <tuomas.marenk@gmail.com>
2022-12-19 14:12:11 +0200

    Tekoaly

---
+++ viikko7/kivi-paperi-sakset/src/kps_tekoaly.py
@@ -0,0 +1,14 @@
+from tuomari import Tuomari
+from tekoaly import Tekoaly
+
+
+class KPSTekoaly:
+    def __init__(self):
+        self._tekoaly = Tekoaly()
+
+    def _toisen_pelaajan_siirto(self, *siirrot):
+        tokan_siirto = self._tekoaly.anna_siirto()
+        print(f"Tietokone valitsi: {tokan_siirto}")
+        return tokan_siirto
+
+
