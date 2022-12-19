553fe721f302b64b5c66718ceacf39a0b9b02857
Tuomas Marenk <tuomas.marenk@gmail.com>
2022-12-19 14:15:01 +0200

    Parempi tekoaly

---
+++ viikko7/kivi-paperi-sakset/src/kps_parempi_tekoaly.py
@@ -0,0 +1,12 @@
+from tekoaly_parannettu import TekoalyParannettu
+
+
+class KPSParempiTekoaly:
+    def __init__(self):
+        self._tekoaly = TekoalyParannettu(10)
+
+    def _toisen_pelaajan_siirto(self, ensimmaisen_siirto):
+        tokan_siirto = self._tekoaly.anna_siirto()
+        print(f"Tietokone valitsi: {tokan_siirto}")
+        self._tekoaly.aseta_siirto(ensimmaisen_siirto)
+        return tokan_siirto
